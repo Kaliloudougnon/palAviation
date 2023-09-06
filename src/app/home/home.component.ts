6d84fb2b077f6634a8fc30331636c8f61ba1d899
kalilou dougnon <dougnonkalilou@gmail.com>
2023-09-06 20:26:15 +0100

    second commit ..

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,42 +17,42 @@ export class HomeComponent implements OnInit {
     {
       icon: "bi bi-bar-chart",
       header: "OUTRIGHT SALE",
-      text: "Some people say that size doesn’t matter, but they don’t have unlimited access to the world’s\n" +
-        "              largest available aircraft component inventory\n",
+      text: "Some people say that size doesn’t matter, but they don’t have unlimited access to the world’s" +
+        "              largest available aircraft component inventory",
       footer: ""
     },
     {
-      icon: "bi bi-gear",
-      header: "LOANS AND REPAIR MANAGEMENT",
-      text: "For many airlines component repair can be a stressful business. Dealing with workshops,\n" +
+      icon: "bi bi-arrow-repeat",
+      header: "LOANS & REPAIR MANAGEMENT",
+      text: "For many airlines component repair can be a stressful business. Dealing with workshops," +
         "              tackling tough timescales, negotiating deals and delivering the goods on",
       footer: ""
     },
     {
       icon: "bi bi-speedometer2",
-      header: "Standard Exchange or Flat Exchange",
-      text: "Pal Aviation helping you save space and no need to maintain high cost units at your\n" +
+      header: "STANDARD EXCHANGE OR FLAT EXCHANGE",
+      text: "Pal Aviation helping you save space and no need to maintain high cost units at your" +
         "              inventories. All the exchange units are provided",
       footer: ""
     },
     {
       icon: "bi bi-people",
-      header: "24/7 AOG SERVICE",
-      text: "Our AOG team is available 24/7/365. We stock\n" +
+      header: "AOG SERVICE",
+      text: "Our AOG team is available 24/7. We stock" +
         "              thousands of line items of airworthy material that can always be dispatched within an hour.",
       footer: ""
     },
-    {
+    /*{
       icon: "bi bi-shop",
       header: "CONSUMABLES AND EXPENDABLES",
       text: "Pal Aviation has a large and growing inventory\n" +
         "              of expendables and consumables. Our management program is designed to...",
       footer: ""
-    },
+    },*/
     {
       icon: "bi bi-house-gear",
       header: "LEASING",
-      text: "Pal Aviation’s leasing expertise, and commitment to constantly increasing our pool, provides\n" +
+      text: "Pal Aviation’s leasing expertise, and commitment to constantly increasing our pool, provides" +
         "              you with a unique partner who can help to unlock...",
       footer: ""
     }
